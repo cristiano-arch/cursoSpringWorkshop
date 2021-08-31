e7e3840315bc2b40aa3897d24ff5c08c2491a743
Cristiano Jr <cristianoandre96@gmail.com>
2021-08-31 18:14:50 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/andcris/cursoSpringWorkshop/CursoSpringWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.andcris.cursoSpringWorkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringWorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andcris/cursoSpringWorkshop/CursoSpringWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andcris.cursoSpringWorkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringWorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
