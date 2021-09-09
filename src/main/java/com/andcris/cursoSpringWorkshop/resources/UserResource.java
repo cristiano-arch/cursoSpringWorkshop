1f15e6de4841a962f0ceb3f7c674a98003fe36d0
Cristiano Jr <cristianoandre96@gmail.com>
2021-09-09 15:35:40 -0300

    Obtendo um post por id

--- src/main/java/com/andcris/cursoSpringWorkshop/resources/UserResource.java
+++ src/main/java/com/andcris/cursoSpringWorkshop/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.andcris.cursoSpringWorkshop.resources;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
---
+++ src/main/java/com/andcris/cursoSpringWorkshop/services/PostService.java
@@ -0,0 +1,22 @@
+package com.andcris.cursoSpringWorkshop.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andcris.cursoSpringWorkshop.domain.Post;
+import com.andcris.cursoSpringWorkshop.repositories.PostRepository;
+import com.andcris.cursoSpringWorkshop.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Post.class.getName()));
+	}
+}
