0f5e17e7275e545a2ea203766cbbe0a98807a27a
Cristiano Jr <cristianoandre96@gmail.com>
2021-09-09 16:26:11 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/andcris/cursoSpringWorkshop/config/Instantiation.java
+++ src/main/java/com/andcris/cursoSpringWorkshop/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.andcris.cursoSpringWorkshop.domain.Post;
 import com.andcris.cursoSpringWorkshop.domain.User;
 import com.andcris.cursoSpringWorkshop.dto.AuthorDTO;
+import com.andcris.cursoSpringWorkshop.dto.CommentDTO;
 import com.andcris.cursoSpringWorkshop.repositories.PostRepository;
 import com.andcris.cursoSpringWorkshop.repositories.UserRepository;
 
@@ -34,17 +35,29 @@ public class Instantiation implements CommandLineRunner{
 		User u1 = new User(null, "Cris", "cris@gmail.com");
 		User u2 = new User(null, "William Dyer", "wdyer@gmail.com");
 		User u3 = new User(null, "Herbert West", "hwest@gmail.com");
+		User u4 = new User(null, "And", "and@gmail.com");
 		
-		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
 		
 		Post p1 = new Post(null, sdf.parse("09/08/1930"), "Partiu viagem", "Vou viajar para Antártida. Abraços!", new AuthorDTO(u2));
 		Post p2 = new Post(null, sdf.parse("09/08/1922"), "Bom dia", "Acordei mais um hoje!", new AuthorDTO(u3));
 		Post p3 = new Post(null, sdf.parse("09/08/1922"), "Olá", "Estou em Arkham!", new AuthorDTO(u3));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!",sdf.parse("09/09/2021"), new AuthorDTO(u1));
+		CommentDTO c2 = new CommentDTO("Obrigado!",sdf.parse("09/08/1930"), new AuthorDTO(u2));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",sdf.parse("09/09/2021"), new AuthorDTO(u4));
+		CommentDTO c4 = new CommentDTO("Você também!",sdf.parse("09/08/1922"), new AuthorDTO(u3));
+		CommentDTO c5 = new CommentDTO("Bela cidade!",sdf.parse("09/09/2021"), new AuthorDTO(u1));
+		CommentDTO c6 = new CommentDTO("Lar da Miskatonic Unversity!",sdf.parse("09/09/2021"), new AuthorDTO(u4));
+		
+		p1.getComments().addAll(Arrays.asList(c1, c2));
+		p2.getComments().addAll(Arrays.asList(c3, c4));
+		p3.getComments().addAll(Arrays.asList(c5, c6));
+		
 		postRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
-		u2.setPosts(Arrays.asList(p1));
-		u3.setPosts(Arrays.asList(p2, p3));
+		u2.getPosts().addAll(Arrays.asList(p1));
+		u3.getPosts().addAll(Arrays.asList(p2, p3));
 		
 		userRepository.saveAll(Arrays.asList(u2, u3));
 	}
--- src/main/java/com/andcris/cursoSpringWorkshop/domain/Post.java
+++ src/main/java/com/andcris/cursoSpringWorkshop/domain/Post.java
@@ -1,12 +1,15 @@
 package com.andcris.cursoSpringWorkshop.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.andcris.cursoSpringWorkshop.dto.AuthorDTO;
+import com.andcris.cursoSpringWorkshop.dto.CommentDTO;
 
 @Document(collection="post")
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 
 	public Post() {
 	}
@@ -71,6 +76,14 @@ public class Post implements Serializable{
 
 	public void setAuthorDto(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	@Override
