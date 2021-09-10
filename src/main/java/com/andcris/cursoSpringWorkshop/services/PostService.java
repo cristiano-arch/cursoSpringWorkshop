4449192aeba5b743326e366af6e5e869586a1e70
Cristiano Jr <cristianoandre96@gmail.com>
2021-09-10 15:23:45 -0300

    Consulta simples com @Query

--- src/main/java/com/andcris/cursoSpringWorkshop/services/PostService.java
+++ src/main/java/com/andcris/cursoSpringWorkshop/services/PostService.java
@@ -22,6 +22,6 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitleContainig(String text) {
-		return postRepository.findByTitleContainingIgnoreCase(text);
+		return postRepository.searchTitle(text);
 	}
 }
